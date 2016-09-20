2b7a15abc6b5014d94743d226026ae583b059329
Evgeniy Krupin <ekrupin@maxus.ru>
2016-09-20 13:42:18 +0300

    02/01

--- test/level30/lesson02/task01/Solution.java
+++ test/level30/lesson02/task01/Solution.java
@@ -1,5 +1,7 @@
 package com.javarush.test.level30.lesson02.task01;
 
+import com.javarush.test.level28.lesson10.home01.Character;
+
 /* Осваиваем методы класса Integer
 Используя метод Integer.parseInt(String, int) реализуйте логику метода convertToDecimalSystem,
 который должен переводить переданную строку в десятичное число и возвращать его в виде строки.
@@ -14,6 +16,22 @@ public class Solution {
     }
 
     public static String convertToDecimalSystem(String s) {
-        return s;
+        int redix = 10;
+        String str = s;
+        if (s.substring(0,2).equals("0x")){
+            redix = 16;
+            str = s.substring(2);
+        }
+        else if (s.substring(0,2).equals("0b")){
+            redix = 2;
+            str = s.substring(2);
+        }
+        else if (s.substring(0,1).equals("0")){
+            redix = 8;
+            str = s.substring(1);
+        }
+
+        String num = "" + Integer.parseInt(str, redix);
+        return num;
     }
 }
