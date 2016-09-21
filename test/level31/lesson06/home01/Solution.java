944392dc09990bd182d6302b312e5bd11fb60799
eekrupin <eekrupin@gmail.com>
2016-09-21 22:22:03 +0300

    20160921_2

--- test/level31/lesson06/home01/Solution.java
+++ test/level31/lesson06/home01/Solution.java
@@ -1,6 +1,12 @@
 package com.javarush.test.level31.lesson06.home01;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 /* Добавление файла в архив
 В метод main приходит список аргументов.
@@ -27,10 +33,54 @@ b.txt
 Пользоваться файловой системой нельзя.
 */
 public class Solution {
+    private static Map<ZipEntry, byte[]> map = new HashMap<>();
     public static void main(String[] args) throws IOException {
-        String pathSource = args[0];
-        String pathZip = args[1];
+        String strPathSource = args[0];
+        String strPathZip = args[1];
 
+        File fileZip = new File(strPathZip);
+        putToMap(fileZip);
 
+        File fileSource = new File(strPathSource);
+        putToZip(fileZip, fileSource);
+    }
+
+    private static void putToZip(File fileZip, File fileSource) {
+        ZipEntry newEntry = new ZipEntry("new/"+fileSource.getName());
+        String newEntryName = newEntry.getName();
+        try(ZipOutputStream zipOut = new ZipOutputStream(new FileOutputStream(fileZip))) {
+            for (Map.Entry<ZipEntry, byte[]> entry : map.entrySet()) {
+                String name = entry.getKey().getName();
+                if (!name.equals(newEntryName)) {
+                    zipOut.putNextEntry(new ZipEntry(name));
+                    zipOut.write(entry.getValue());
+                }
+            }
+            ZipEntry entry = newEntry;
+            zipOut.putNextEntry(entry);
+            Files.copy(fileSource.toPath(), zipOut);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void putToMap(File File) {
+        try(ZipInputStream zipInput = new ZipInputStream(new FileInputStream(File))) {
+            ZipEntry zipEntry;
+            while ((zipEntry = zipInput.getNextEntry()) != null) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                byte[] cache = new byte[1024];
+                int count;
+                while ((count = zipInput.read(cache)) != -1) {
+                    byteArrayOutputStream.write(cache, 0, count);
+                }
+                byte[] bytes = byteArrayOutputStream.toByteArray();
+                map.put(zipEntry, bytes);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
