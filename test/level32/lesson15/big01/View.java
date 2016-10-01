29490a0bad9bf690fa22f3819126b6fc06bf2547
eekrupin <eekrupin@gmail.com>
2016-10-01 23:05:06 +0300

    20161001

--- test/level32/lesson15/big01/View.java
+++ test/level32/lesson15/big01/View.java
@@ -1,8 +1,12 @@
 package com.javarush.test.level32.lesson15.big01;
 
 import com.javarush.test.level32.lesson15.big01.listeners.FrameListener;
+import com.javarush.test.level32.lesson15.big01.listeners.TabbedPaneChangeListener;
+import com.javarush.test.level32.lesson15.big01.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.UndoManager;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -15,6 +19,22 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane();
     private JTextPane htmlTextPane = new JTextPane();
     private JEditorPane plainTextPane = new JEditorPane();
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
+
+    public View() {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (IllegalAccessException e) {
+            ExceptionHandler.log(e);
+        } catch (InstantiationException e) {
+            ExceptionHandler.log(e);
+        } catch (UnsupportedLookAndFeelException e) {
+            ExceptionHandler.log(e);
+        } catch (ClassNotFoundException e) {
+            ExceptionHandler.log(e);
+        }
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
@@ -41,11 +61,24 @@ public class View extends JFrame implements ActionListener {
     }
 
     public void initMenuBar(){
-
+        JMenuBar jMenuBar = new JMenuBar();
+        MenuHelper.initFileMenu(this, jMenuBar);
+        MenuHelper.initEditMenu(this, jMenuBar);
+        MenuHelper.initStyleMenu(this, jMenuBar);
+        MenuHelper.initAlignMenu(this, jMenuBar);
+        MenuHelper.initColorMenu(this, jMenuBar);
+        MenuHelper.initFontMenu(this, jMenuBar);
+        MenuHelper.initHelpMenu(this, jMenuBar);
+        getContentPane().add(jMenuBar, BorderLayout.NORTH);
     }
 
     public void initEditor(){
-
+        htmlTextPane.setContentType("text/html");
+        tabbedPane.addTab("HTML", new JScrollPane(htmlTextPane));
+        tabbedPane.addTab("Текст", new JScrollPane(plainTextPane));
+        tabbedPane.setPreferredSize(new Dimension(800, 600));
+        tabbedPane.addChangeListener(new TabbedPaneChangeListener(this));
+        getContentPane().add(tabbedPane, BorderLayout.CENTER);
     }
 
     public void initGui(){
@@ -56,5 +89,29 @@ public class View extends JFrame implements ActionListener {
 
     public void selectedTabChanged() {
 
+    }
+
+    public boolean canUndo() {
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
+
+    public void undo(){
+        undoManager.undo();
+    }
+
+    public void redo(){
+        undoManager.redo();
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
+    }
+
+    public void resetUndo(){
+        undoManager.discardAllEdits();
     }
 }
